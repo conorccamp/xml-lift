64bf21e048561316786577e53423d3cc53772891
cctextur <conor@textur.com>
2016-03-14 19:19:05 -0700

    added key donation functionality

--- xml_parser.py
+++ xml_parser.py
@@ -38,7 +38,7 @@ class Parser(object):
                 element_class = None
             # create the element class if this is the first time we've seen it
             if not element_class:
-                element_class = DiscogsEntity(element, self.parent_class)
+                element_class = XmlEntity(element, self.parent_class)
                 element_meta.update({'class':element_class})
             self.parent_class = element_class
         else:
@@ -54,35 +54,92 @@ class Parser(object):
 
 class XmlEntity(object):
     def __init__(self, xml_element, parent_class):
-        self.xml_element = xml_element
+        self.element = xml_element
         self.name = xml_element.tag
         self.parent = parent_class
-        self.add_to_parent()
         self.values = []
         self.children = []
+        self.promote_key = False
+        self.add_to_parent()
+        self._init_donor_key()
+
+    def _init_donor_key(self):
+        '''Inherit parent donor key'''
+        if self.parent:
+            self.donor_key = self.parent.donor_key
+        else:
+            self.donor_key = None
+        self.donor = False
 
     def add_to_parent(self):
+        '''Appends current class to the Parent.children list'''
         if self.parent:
-            self.parent.children.append(self)
+            self.parent._add_child_class(self)
+
+    def _add_child_class(self, child):
+        self.children.append(child)
+        self._ask_for_key(child)
+
+    def _ask_for_key(self, child):
+        '''Asks child to set the donor key if it's name matches the provided key'''
+        if args.keys and child.name in args.keys:
+            child.promote_key = True
+            self.donor = True
 
     def add_value(self, xml_element):
         '''Adds element.text as a value if it is not an empty string'''
         if xml_element.text and xml_element.text.strip():
             self.values.append(xml_element.text)
 
-    def _reset_values(self):
-        self.values = []
+    def _add_donor_key(self):
+        '''Adds donor key to parent and siblings and outputs elements on the same level if they are waiting'''
+        if self.promote_key:
+            self.donor_key = {self.parent.name+'_'+self.name: self.values[0]}
+            self.parent.donor_key = self.donor_key
+            for sibling in self.parent.children: sibling.donor_key = self.donor_key
+            # output values for sibling elements that were waiting on the donor key
+            map(lambda sib: sib._output_values(), self.parent.children)
 
-    def _return_values(self):
-        '''Criteria for when values should be output to the stream.
-           Default is for the top level element (this is inefficient).'''
-        if args.split:
-            if self.name in args.split:
+    def _donor_key_check(self):
+        '''Check to see if we need to wait for the parent to add a donor key'''
+        if args.keys:
+            # if this is the class donating the key it doesn't need to wait for an update
+            if self.donor:
                 return True
+            # if parent donor key has been added, update all values and return True
+            elif self.donor_key:
+                if self.entity_type == 'array' and self.values:
+                    for val in self.values[0]: val.update(self.donor_key)
+                else:
+                    for val in self.values: val.update(self.donor_key)
+                return True
+            # otherwise we need to wait for the donor key
+            else:
+                return False
+        # Always pass the test if no key option was received
+        else:
+            return True
+
+    def _output_criteria_check(self):
+        '''Criteria for when values should be output to the stream.'''
+        if args.tags:
+            # if the element name matches the split tags and the donor key check passes
+            if self.name in args.tags and self._donor_key_check():
+                return True
+        # Default is for the top level element (this is inefficient).
         else:
             if self.parent is None:
                 return True
         return False
+
+    def _output_values(self):
+        if self._output_criteria_check() == True and self.values:
+            sys.stdout.write(json.dumps({self.name: self.values[0]})+'\n')
+            self._reset_values()
+
+    def _reset_values(self):
+        self.values = []
+        self.donor_key = None
 
     def _set_entity_type(self):
         if len(self.children) == 0:
@@ -98,6 +155,7 @@ class XmlEntity(object):
         self._set_entity_type()
         if self.entity_type == 'pair':
             if len(self.values) == 0: self.values = [None]
+            self._add_donor_key()
             return
         elif self.entity_type == 'array':
             values = []
@@ -117,40 +175,55 @@ class XmlEntity(object):
 
         # reset child values since we're done with this instance
         map(lambda child: child._reset_values(), self.children)
+        # output values for this element if it meets the requirements
+        self._output_values()
 
-        if self._return_values():
-            sys.stdout.write(json.dumps({self.name: self.values[0]})+'\n')
-            self._reset_values()
-            
 
-class DiscogsEntity(XmlEntity):
-    def __init__(self, xml_element, parent_class):
-        super(XmlEntity, self).__init__()
-        self.name = xml_element.tag
-        self.parent = parent_class
-        self.add_to_parent()
-        self.values = []
-        self.children = []
-
-#    def _return_values(self):
-#        if self.name in args.split:
-#            return True
-#        else:
-#            return False
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("filepath")
-    parser.add_argument("-s", "--split", nargs="+", 
-            help="element names that will be split and output separately.")
+    subparsers = parser.add_subparsers(dest="subparser_name", help="sub-command help")
+
+    # parser for the 'parse' command
+    full_parser = subparsers.add_parser('full',
+            help="Converts the full XML document to JSON")
+    full_parser.add_argument("-f", "--filepath", required=True,
+            help="path to file to be processed")
+
+    # parser for the 'split' command
+    split_parser = subparsers.add_parser('split',
+            help="outputs stream of elements based on provided tags")
+    split_parser.add_argument("-f", "--filepath", required=True,
+            help="path to file to be processed")
+    split_parser.add_argument("-t", "--tags", nargs="+", required=True,
+            help="element tags that will be split and output separately.")
+    split_parser.add_argument("-k", "--keys", nargs="+",
+            help="Values that will be donated from parents and serve as foreign keys for split elements")
     args = parser.parse_args()
     
+    if args.subparser_name == 'full':
+        args.tags = None
+        args.keys = None
     filepath = args.filepath
     parser = Parser(filepath)
     for event, element in parser.iterparser:
         parser.parse_xml(event, element)
 
 
+#class DiscogsEntity(XmlEntity):
+#    def __init__(self, xml_element, parent_class):
+#        super(XmlEntity, self).__init__()
+#        self.name = xml_element.tag
+#        self.parent = parent_class
+#        self.add_to_parent()
+#        self.values = []
+#        self.children = []
+#
+#    def _return_values(self):
+#        if self.name in args.split:
+#            return True
+#        else:
+#            return False
 
 
 
